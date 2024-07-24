c346d68f2ebcdc4f78886bae205ca40f9a314548
Federico Paolucci <federico@iPhonediGiorgio.localdomain>
2024-07-24 17:25:03 +0200

    updatescript

--- script.sh
+++ script.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-PISERVER=$(kubectl config view --minify | grep server | cut -f 2- -d ":" | tr -d " ")
-TOKEN=$(kubectl describe secret explorer-token | grep -E '^token' | cut -f2 -d':' | tr -d " ")
-
-curl $APISERVER/apis/apps/v1/namespaces/default/deployments/myappname11 --header "Authorization: Bearer $TOKEN" --insecure -o deployment.json
-
 HAS_LIVENESS_PROBE=$(jq '.spec.template.spec.containers[] | select(has("livenessProbe")) | .name' deployment.json)
 
 HAS_READINESS_PROBE=$(jq '.spec.template.spec.containers[] | select(has("readinessProbe")) | .name' deployment.json)
