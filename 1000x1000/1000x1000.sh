fa61271cc5746dd0ab311b3ead6d7cc37ebb3fed
federicopao <federico.paolucci@sourcesense.com>
2024-07-25 12:07:03 +0200

    Update 1000x1000.sh

--- 1000x1000/1000x1000.sh
+++ 1000x1000/1000x1000.sh
@@ -2,18 +2,19 @@
 
 date1=$(date +%s)
 
-#I create a balanced search tree recursively
-mkdir -p test/{0..5}/{0..5}/{0..5}
-for i in {0..30};
+# Struttura ad albero per le cartelle
+# 10^3 + 10^2 + 10^1 + radice "test" = 1111 cartelle
+mkdir -p test/{0..9}/{0..9}/{0..9}
+for i in {0..100};
 do
-  #for each node I create a random file
-  #repeat this action if you want more file in each node
-  #for bash use:
-  #shopt -s globstar
-  #for d in **/*/; do touch -- "$d/file"; done
   for d in **/*(/); do dd if=/dev/random  bs=1024 count=40 2>/dev/null|base64 >"$d/t"$i"" && truncate -s 40K "$d/t"$i""; done
+
+  # per shell bash usare ../. oppure abilitare * con il comando shopt:
+  # shopt -s globstar
+  # for d in **/*/; do touch -- "$d/file"; done
 done
 
+# comprimo, invio sulla vm, decomprimo sulla vm in ssh
 tar -cjf test.tar.bz2 test
 scp -r /Users/federico/esercizio/test.tar.bz2 utente1@192.168.39.130:/home/utente1
 ssh utente1@192.168.39.130 "tar -xjf test.tar.bz2"
@@ -26,14 +27,21 @@ cd /Users/federico/esercizio/test
 
 for j in {0..5}
 do
-  #set two variable, one for the path of the directory, one for choose the file in the directory
-  rdmdir=$((RANDOM % 3))
-  rdmfile=$((RANDOM % 30))
 
+  cd /Users/federico/esercizio/test
+  # variabile usata per indicare il path del file che andrà modificato
+  p=/Users/federico/esercizio/test/
+  # usata per scegliere il livello di profondità dell'albero
+  rdmdir=$((RANDOM % 3))
+  # variabile che seleziona quale dei 100 file nella cartella selezionata dovrà essere modificato  
+  rdmfile=$((RANDOM % 99))
+
+    # se il livello è diverso da 0 bisogna creare il path
     if [ "$rdmdir" != "0" ]
     then
       for x in {0..$rdmdir};
       do
+        #per ogni livello ho diverse cartelle tra cui scegliere, ogni livello ha 10^(numero di livello) directory in cui scendere
         rdmdir2=$((RANDOM % 3))
         p="$p$rdmdir2"/
       done
@@ -42,14 +50,11 @@ do
   echo $p
   echo "$p"t"$rdmfile"
 
-  #modify the file
-  cat /dev/null > t"$rdmfile"
+  #modifico il file
+  cat /dev/null > "$p"t"$rdmfile"
 
-  #return at the main path
-  cd /Users/federico/esercizio/test
-
-  p=/Users/federico/esercizio/test/
 done
 
-#rsync overwrite only the modify files
+#aggiorno i dati sulla vm con le modifiche apportate
+#rsync sovrascrive solo i file aggiornati
 rsync -av /Users/federico/esercizio/test/ utente1@192.168.39.130:/home/utente1/test/
